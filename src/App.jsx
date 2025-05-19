e3f683abf5aeafcfea24a1c21452fd27f425168b
maikonsm7 <maikonsm7@gmail.com>
2025-05-18 21:15:17 -0400

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +7,8 @@ function App() {
   return (
     <>
      <h1>Projeto React + Vite no GitHub Pages</h1>
+     <p>Contador: {count}</p>
+     <button onClick={()=>setCount(v => v + 1)}>Clique aqui</button>
     </>
   )
 }
